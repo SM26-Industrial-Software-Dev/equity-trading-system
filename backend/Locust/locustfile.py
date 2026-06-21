47bcc868aa86c2755f7c9d9489524efbac2f111c
Sean Alter <seanalter1@gmail.com>
2026-06-21 15:50:24 -0400

    ➕ Add script to spin up cluster
    ➕ Add reload script
    
    ♻️ Fix reload to not use non-existant ns
    
    ♻️ Use new ns
    
    ♻️ Fix dash
    
    📝 Update DEVELOPER Docs

--- backend/Locust/locustfile.py
+++ backend/Locust/locustfile.py
@@ -1,17 +1,9 @@
 from locust import HttpUser, task, between
 
 class EquityTradingUser(HttpUser):
+    # Simulates a user waiting 1 to 3 seconds between actions
     wait_time = between(1, 3)
 
-    @task(3) # The '3' makes it 3x more likely to run this task
-    def load_docs(self):
+    @task
+    def check_health(self):
         self.client.get("/docs")
-
-    @task(2)
-    def check_positions(self):
-        # This will likely return a 401, but it generates awesome error rate metrics!
-        self.client.get("/positions") 
-
-    @task(1)
-    def attempt_login(self):
-        self.client.post("/login", json={"username": "test", "password": "password"})
